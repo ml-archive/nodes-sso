64bbc251060aab05b800a91f20d58a13de4beee2
Casper Rasmussen <cr@nodes.dk>
2017-01-11 17:24:13 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "AdminPanelNodesSSO",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-        .Package(url: "https://github.com/nodes-vapor/admin-panel", majorVersion: 0)
+        .Package(url: "https://github.com/nodes-vapor/admin-panel.git", majorVersion: 0)
     ]
 )
