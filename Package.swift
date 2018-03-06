80445c8d2446ea341a75cfce591c28f770c646cf
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-03-06 15:25:03 +0100

    Bump admin panel provider to 0.5.x

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "AdminPanelNodesSSO",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/nodes-vapor/admin-panel-provider.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/nodes-vapor/admin-panel-provider.git", majorVersion: 0, minor: 5)
     ]
 )
