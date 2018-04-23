9d2dda540cafd4a3a16ee2a226456f4c6bcf551d
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-04-23 14:35:42 +0200

    Update to Admin Panel 0.6.0 + Swift 4.1

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "AdminPanelNodesSSO",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/nodes-vapor/admin-panel-provider.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/nodes-vapor/admin-panel-provider.git", majorVersion: 0, minor: 6)
     ]
 )
