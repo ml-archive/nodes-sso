84dcbb86cfb367a5f3ac98b4a965349dc2672883
Steffen D. Sommer <steffendsommer@icloud.com>
2018-07-05 16:54:26 +0200

    Make the login path dynamic

--- Sources/NodesSSO/NodesSSOProvider.swift
+++ Sources/NodesSSO/NodesSSOProvider.swift
@@ -10,11 +10,17 @@ public final class NodesSSOProvider<U: NodesSSOAuthenticatable>: Provider {
     }
 
     public func register(_ services: inout Services) throws {
+        try services.register(MutableLeafTagConfigProvider())
         services.register(config)
+        services.register(NodesSSOConfigTagData(loginPath: config.loginPath))
     }
 
     public func didBoot(_ container: Container) throws -> EventLoopFuture<Void> {
         try routes(container.make(), config: container.make(), controller: NodesSSOController<U>())
+
+        let tags: MutableLeafTagConfig = try container.make()
+        tags.use(NodesSSOConfigTag(), as: "nodessso:config")
+
         return .done(on: container)
     }
 }
